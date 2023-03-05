80df02eb96b041d3835ab63bb0283d783a31b7b9
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2023-03-05 17:34:57 +0100

    Working on show list of images from fireBase by categorie

--- src/components/AutComponents/AutViewImageComponent.js
+++ src/components/AutComponents/AutViewImageComponent.js
@@ -12,8 +12,50 @@ import { ref, listAll, getDownloadURL} from 'firebase/storage';
 const AutViewImageComponent = () => {
   
   const [imageList, setImageList] = useState([]);
-  const imageListRef = ref(storage,"Professionals/" )
+  
+  //HOOKS FOR SELECTBOX FIELD
+  const [selectValue, setSelectValue] = useState();
+  
+  let storeRoute= "/";
 
+  
+  //GET THE VALUE OF SELECTBOX
+  const handleSelectBoxValue = (newValue) => {
+    setSelectValue(newValue);
+    storeRoute= newValue + '/';
+    console.log(storeRoute);
+  }
+
+  switch (selectValue) {
+    case "":
+      storeRoute="/"
+      break;
+    case 'Professionals':
+      storeRoute='Professionals/'
+      break;
+    case 'Familiar':
+      storeRoute='Familiar/'
+      break;
+    case 'Weddings':
+      storeRoute='Weddings/'
+      break;
+    case 'Birthday':
+      storeRoute='Birthday/'
+      break;
+    case 'Fashion':
+      storeRoute='Fashion/'
+      break;
+    case 'Pregnancy':
+      storeRoute='Pregnancy/'
+      break;  
+    default:
+      storeRoute="/"
+      break;
+    }
+  
+  const imageListRef = ref(storage,'Professionals/')
+  
+    //GET IMAGES FROM FIRESTORE
   useEffect(() => {
     listAll(imageListRef).then((response) => {
       response.items.forEach((item) => {
@@ -29,12 +71,12 @@ const AutViewImageComponent = () => {
       <div className='flex justify-center'>
         <h3 className='text-justify text-[1.3rem] mt-[1rem] mr-5'>Plaese select a categorie of image to show you</h3>
         <div className='mt-3'>
-          <SelectBox />
+          <SelectBox onValueChange={handleSelectBoxValue}/>
         </div>
       </div>
       <div className=' mt-[3rem] grid grid-cols-4 gap-4'>
-        {imageList.map((url) => {
-          return <ShowImage url={url}/>
+        {imageList.map((url,index) => {
+          return <ShowImage key={index} url={url}/>
         })}
       </div>
     
