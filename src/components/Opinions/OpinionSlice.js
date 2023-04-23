74f92781b8d8def78c8d359ca09aa13783849fb1
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2023-04-23 18:21:25 +0200

    Changes on Testimonials

--- src/components/Opinions/OpinionSlice.js
+++ src/components/Opinions/OpinionSlice.js
@@ -1,12 +1,10 @@
 import Slider from 'react-slick'
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-
-import { Imagen1, Imagen2, Imagen3, Imagen4, Imagen5, Imagen6 } from '../../img/opinions';
+import Opinions from '../../texts/opinionText';
 
 const OpinionSlice = () => {
-    const images = [Imagen1, Imagen2, Imagen3, Imagen4, Imagen5, Imagen6];
-
+   
     const settings = {
         dots: true,
         infinite: true,
@@ -57,30 +55,24 @@ const OpinionSlice = () => {
     }
   return (
     <>
-        <Slider {...settings}>
-          
-            
-                
-                    {
-                        images.map((image, i) => (
-                            <div className='h-[15rem] mb-[8rem]' key={i}>
-                                <div className='flex justify-center' >
-                                    <div 
-                                        className='bg-no-repeat bg-[center_-8rem] rounded-full w-[10rem] h-[10rem]' 
-                                        style={{ backgroundImage: `url(${image})`}}
-                                    />
-                                </div>
-                                <div className='mx-8'>
-                                    <h4 className='text-[1.2rem] text-center mt-5 font-bold'>Katerina Kuriska</h4> 
-                                    <p className='text-justify mt-5 text-lg'>"Thank you so much for capturing such beautiful moments. Your talent behind the lens is truly inspiring."</p>
-                                </div>
-                            </div>
+        <Slider {...settings}> 
+                  {
+                      Opinions.map((opinion, i) => (
+                          <div className='h-[15rem] mb-[8rem]' key={i}>
+                              <div className='flex justify-center' >
+                                  <div 
+                                      className='bg-no-repeat bg-[center_-8rem] rounded-full w-[10rem] h-[10rem]' 
+                                      style={{ backgroundImage: `url(${opinion.img})`}}
+                                  />
+                              </div>
+                              <div className='mx-8'>
+                                  <h4 className='text-[1.2rem] text-center mt-5 font-bold'>{opinion.persone}</h4> 
+                                  <p className='text-justify mt-5 text-lg'>"{opinion.opinion}"</p>
+                              </div>
+                          </div>
 
-                        ))
-                    }
-
-              
-                   
+                      ))
+                  }
         </Slider>
       </>
   )
---
+++ src/texts/opinionText.js
@@ -0,0 +1,34 @@
+import {Imagen1, Imagen2, Imagen3, Imagen4, Imagen5, Imagen6 } from '../img/opinions/'
+const Opinions =[
+    {
+        persone:'Anastasiya Ivanova',
+        img:Imagen1,
+        opinion:'Lusine is a fantastic photographer. She really has an eye for capturing the perfect shot and made us feel so comfortable during our session.',
+    },
+    {
+        persone:'Maria Petrova',
+        img:Imagen2,
+        opinion:'I absolutely love the photos that Lusine took of my family. She captured all of our personalities and the memories we made that day beautifully.',
+    },
+    {
+        persone:'Oksana Kovalenko',
+        img:Imagen3,
+        opinion:'Working with Lusine was such a pleasure. She was professional, patient and really took the time to understand what we were looking for in our photos.',
+    },
+    {
+        persone:'Yana Tkachenko',
+        img:Imagen4,
+        opinion:'Lusine is simply amazing. She took my headshot for my business and I have received so many compliments on how professional and stunning it looks.',
+    },
+    {
+        persone:'Kateryna Kozlova',
+        img:Imagen5,
+        opinion:'I highly recommend Lusine for any photography needs. She has a talent for capturing natural, candid moments that truly tell a story.',
+    },
+    {
+        persone:'Daria Zaitseva',
+        img:Imagen6,
+        opinion:'Lusine is a true artist. She takes the time to get to know her clients and the end result is always a masterpiece.',
+    },
+]
+export default Opinions;
